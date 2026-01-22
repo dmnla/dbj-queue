9000880c21a906e63ba5c31fa37580e363b58826
domdomdom <dmanuela72@gmail.com>
2026-01-22 16:33:38 +0700

    Update firebaseConfig.ts

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -1,15 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace with your actual Firebase project configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBm9-55KfxxbnBC89xBbXfoK2AHGzI2KoY",
+  authDomain: "dbj-queue.firebaseapp.com",
+  projectId: "dbj-queue",
+  storageBucket: "dbj-queue.firebasestorage.app",
+  messagingSenderId: "29922630063",
+  appId: "1:29922630063:web:c63e5883cd9aaa3108375a",
+  measurementId: "G-QJ00PN3SDF"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
